4d3c846e7a00a876ed6e3d9a3fb374275fb13c06
Renan Borges <renanvcb@gmail.com>
2022-06-18 15:12:24 -0300

    summaries updated

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -12,21 +12,21 @@ export function Summary() {
           <p>Entradas</p>
           <img src={incomeImg} alt="Entradas" />
         </header>
-        <strong>R$ 1000,00</strong>
+        <strong>R$ 12.000,00</strong>
       </div>
       <div>
         <header>
           <p>Saídas</p>
           <img src={outcomeImg} alt="Saídas" />
         </header>
-        <strong>R$ 300,00</strong>
+        <strong>R$ 800,00</strong>
       </div>
       <div className="highlight-background">
         <header>
           <p>Total</p>
           <img src={totalImg} alt="Total" />
         </header>
-        <strong>R$ 700,00</strong>
+        <strong>R$ 11.200,00</strong>
       </div>
     </Container>
   );
